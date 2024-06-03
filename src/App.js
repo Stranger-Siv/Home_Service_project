6fcf7c2db413492acb01ee46740525ba5307338e
Sivram Pradhan <programmersiv21@gmail.com>
2024-06-03 07:36:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import About from "./pages/About"
+import Signup from "./pages/Signup"
+import Dashboard from "./pages/Dashboard"
+import { useEffect, useState } from 'react'
+import PrivateRoute from "./components/PrivateRoute";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import ProductTemp from "./components/ProductTemp";
+
+
+function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+ 
+
+  return (
+    <div className="w-screen h-screen flex flex-col bg-gray-900 text-white">
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
+
+
+      <Routes>
+
+        <Route path="/" element= {<Home isLoggedIn={isLoggedIn}/>} />
+        <Route path="/login" element = {<Login  setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/signup" element={<Signup  setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/dashboard" element = {
+          <PrivateRoute isLoggedIn={isLoggedIn}>
+              <Dashboard/>
+          </PrivateRoute>
+        } />
+        <Route path="/about" element= {<About/>} />
+        <Route path="/contact" element= {<Contact/>} />
+        <Route path="/productPage" element= {
+           <PrivateRoute isLoggedIn={isLoggedIn}>
+              <Product/>
+           </PrivateRoute>
+        } />
+        <Route path="/temp" element={<ProductTemp/>}/>
+
+      </Routes>
+    </div>
+    )
+}
+
+export default App;
